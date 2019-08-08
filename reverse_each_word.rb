02bcf6e59af376a09d1b9863480717ae9bd75118
Christopher Kennedy <chris94kennedy@gmail.com>
2019-08-08 14:04:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,12 @@
 def reverse_each_word(sentence)
+<<<<<<< HEAD
   array_of_split_string = []
   reversed_words = []
   array_of_split_string = sentence.split
   array_of_split_string.each {|words| reversed_words << words.reverse!}
   reversed_words.join(" ")
 end
-#first you create an empty array to fill with the passed sentence string, and then an empty array (reversed_words) to then fill with enumerated (all elements worked upon) 'array_of_split_string'. Then, you have split the passed sentence into a an array with .split turning each word into an element. Next, you have used the .each method to work on each element, and then given a tag to the individual elements as 'words'. THen you asked the enumerator to reverse each element ('words') and add that in order of the original elements (not reversing the order itself) to the array reversed_words, using << and .reverse!. Then you used .join to turn it back into a string and asked .join to put a space (" ") between each element so that it reads like a sentence. 
+#first you created an empty array to fill with the passed sentence string, and then an empty array (reversed_words) to then fill with enumerated (all elements worked upon) 'array_of_split_string'. Then, you have split the passed sentence into a an array with .split turning each word into an element. Next, you have used the .each method to work on each element, and then given a tag to the individual elements as 'words'. THen you asked the enumerator to reverse each element ('words') and add that in order of the original elements (not reversing the order itself) to the array reversed_words, using << and .reverse!. Then you used .join to turn it back into a string and asked .join to put a space (" ") between each element so that it reads like a sentence. 
 
 def reverse_each_word(sentence)
   array_of_split_string = []
@@ -16,3 +17,11 @@ def reverse_each_word(sentence)
 end
 
 
+=======
+  reversed = []
+  newarray = sentence.split
+  newarray.map {|words| words.reverse}
+  reversed << newarray.join
+
+end 
+>>>>>>> 99795b56555e8a644379505e55699846ce9ee691
